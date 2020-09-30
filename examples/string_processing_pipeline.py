d5206fdcffe1a1dccef520b8da1e16861b5ea8ec
Samuel Lampa <samuel.lampa@rilnet.com>
2020-09-30 18:09:11 +0200

    Put main() in top of file

--- examples/string_processing_pipeline.py
+++ examples/string_processing_pipeline.py
@@ -1,61 +1,5 @@
 import asyncio
 
-class HiSayer:
-    out_lines = asyncio.Queue()
-
-    async def run(self):
-        for i in range(20):
-            self.out_lines.put_nowait(f'Hi hi for the {i+1}:th time...')
-            self.out_lines.task_done()
-
-
-class StringSplitter:
-    in_lines = asyncio.Queue()
-    out_leftpart = asyncio.Queue()
-    out_rightpart = asyncio.Queue()
-
-    async def run(self):
-        while not self.in_lines.empty():
-            s = await self.in_lines.get()
-            self.out_leftpart.put_nowait(s[:int(len(s)/2)])
-            self.out_rightpart.put_nowait(s[int(len(s)/2):])
-
-class LowerCaser:
-    in_lines = asyncio.Queue()
-    out_lines = asyncio.Queue()
-
-    async def run(self):
-        while not self.in_lines.empty():
-            s = await self.in_lines.get()
-            self.out_lines.put_nowait(s.lower())
-
-class UpperCaser:
-    in_lines = asyncio.Queue()
-    out_lines = asyncio.Queue()
-
-    async def run(self):
-        while not self.in_lines.empty():
-            s = await self.in_lines.get()
-            self.out_lines.put_nowait(s.upper())
-
-class StringJoiner:
-    in_leftpart = asyncio.Queue()
-    in_rightpart = asyncio.Queue()
-    out_lines = asyncio.Queue()
-
-    async def run(self):
-        while not self.in_leftpart.empty() or not self.in_rightpart.empty():
-            leftpart = await self.in_leftpart.get()
-            rightpart = await self.in_rightpart.get()
-            self.out_lines.put_nowait(f'{leftpart}{rightpart}')
-
-class Printer:
-    in_lines = asyncio.Queue()
-
-    async def run(self):
-        while not self.in_lines.empty():
-            s = await self.in_lines.get()
-            print(f'Printer got line: {s}')
 
 def main():
     loop = asyncio.get_event_loop()
@@ -91,5 +35,67 @@ def main():
     loop.run_until_complete(printer.run())
 
 
-if __name__ == '__main__':
+class HiSayer:
+    out_lines = asyncio.Queue()
+
+    async def run(self):
+        for i in range(20):
+            self.out_lines.put_nowait(f"Hi hi for the {i+1}:th time...")
+            self.out_lines.task_done()
+
+
+class StringSplitter:
+    in_lines = asyncio.Queue()
+    out_leftpart = asyncio.Queue()
+    out_rightpart = asyncio.Queue()
+
+    async def run(self):
+        while not self.in_lines.empty():
+            s = await self.in_lines.get()
+            self.out_leftpart.put_nowait(s[: int(len(s) / 2)])
+            self.out_rightpart.put_nowait(s[int(len(s) / 2) :])
+
+
+class LowerCaser:
+    in_lines = asyncio.Queue()
+    out_lines = asyncio.Queue()
+
+    async def run(self):
+        while not self.in_lines.empty():
+            s = await self.in_lines.get()
+            self.out_lines.put_nowait(s.lower())
+
+
+class UpperCaser:
+    in_lines = asyncio.Queue()
+    out_lines = asyncio.Queue()
+
+    async def run(self):
+        while not self.in_lines.empty():
+            s = await self.in_lines.get()
+            self.out_lines.put_nowait(s.upper())
+
+
+class StringJoiner:
+    in_leftpart = asyncio.Queue()
+    in_rightpart = asyncio.Queue()
+    out_lines = asyncio.Queue()
+
+    async def run(self):
+        while not self.in_leftpart.empty() or not self.in_rightpart.empty():
+            leftpart = await self.in_leftpart.get()
+            rightpart = await self.in_rightpart.get()
+            self.out_lines.put_nowait(f"{leftpart}{rightpart}")
+
+
+class Printer:
+    in_lines = asyncio.Queue()
+
+    async def run(self):
+        while not self.in_lines.empty():
+            s = await self.in_lines.get()
+            print(f"Printer got line: {s}")
+
+
+if __name__ == "__main__":
     main()
